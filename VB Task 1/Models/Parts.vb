fe7ec9970223d91bc481c2bda332850e9c871a6f
Caleb Guinn <drake19cg@gmail.com>
2018-12-17 21:22:12 -0600

    Modify Views to display remove irrelvant links

--- VB Task 1/Models/Parts.vb
+++ VB Task 1/Models/Parts.vb
@@ -1,11 +1,15 @@
-﻿Imports System.ComponentModel.DataAnnotations.Schema
+﻿Imports System.ComponentModel.DataAnnotations
+Imports System.ComponentModel.DataAnnotations.Schema
 Imports System.Data.Entity
 Namespace Models
     Public Class Parts
         'Primary Key
+
         Public Property ID() As Integer
+        <Display(Name:="Part Number")>
         Public Property PartNumber() As String
         Public Property Description() As String
+        <Display(Name:="Price")>
         Public Property PriceID() As Integer
         <ForeignKey("PriceID")>
         Public Property PriceRef() As Price
